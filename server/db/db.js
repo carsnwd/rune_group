c5b03b2b7b3f9268ba8f2990f80191cff02011f8
Carson <carsnwd@gmail.com>
2018-08-12 00:51:53 -0400

    Reorganizing the file structure

--- server/db/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var url = 'mongodb://localhost:27017/rune-group';
-mongoose.connect(url);
-mongoose.Promise = global.Promise;
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error: '));
-
-module.exports = db;
